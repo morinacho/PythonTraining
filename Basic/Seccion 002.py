88a92d59f5b8a9c8cae95cdf1a1c17f41e8e116e
mirai <mirai@mirai.mirai.net>
2022-05-08 10:14:22 -0300

    Starting with the basic

---
+++ Basic/Seccion 002.py
@@ -0,0 +1,9 @@
+#Variables
+miVariable = "Hola desde python"
+print(miVariable)
+numero = 12
+print(numero)
+# En python no es necesario declarar el tipo de la variable
+numero2 = 13
+resultado = numero + numero2
+print(resultado)
