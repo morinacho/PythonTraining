cd11283dc8f1b46b3a593d991f4ebe84fca0ad81
mirai <mirai@mirai.mirai.net>
2022-05-23 18:34:39 -0300

    Seccion 4

---
+++ 01 Basic/Seccion 004.py
@@ -0,0 +1,14 @@
+#Operadores
+#Aritmeticos
+a = 2
+b = 3
+suma = a+b #Lo mismo para - * / 
+print(f'suma: {suma}')
+
+# Division entera, potencia y modulo
+division = b // a
+print(f'Division con resultado entero {division}')
+potencia = a ** b
+print(f'Potencia {potencia}')
+modulo = a % b
+print(f'modulo {modulo}')
