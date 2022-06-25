25168241b28fcddad81c8a6187aec39018f9f915
Ismael Ignacio Morinigo <morinacho@gmail.com>
2022-06-25 16:30:42 -0300

    Delete Basic directory

--- Basic/Seccion 002.py
+++
@@ -1,9 +0,0 @@
-#Variables
-miVariable = "Hola desde python"
-print(miVariable)
-numero = 12
-print(numero)
-# En python no es necesario declarar el tipo de la variable
-numero2 = 13
-resultado = numero + numero2
-print(resultado)
